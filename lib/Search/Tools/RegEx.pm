b34f1f68bbcb4ac4c4f82167b6770ddeab2cd44d
Peter Karman <perl@peknet.com>
2012-08-08 03:40:47 +0000

    Import of KARMAN/Search-Tools-0.76 from CPAN.
    gitpan-cpan-distribution: Search-Tools
    gitpan-cpan-version:      0.76
    gitpan-cpan-path:         KARMAN/Search-Tools-0.76.tar.gz
    gitpan-cpan-author:       KARMAN
    gitpan-cpan-maturity:     released

--- lib/Search/Tools/RegEx.pm
+++ lib/Search/Tools/RegEx.pm
@@ -6,7 +6,7 @@ use Carp;
 
 #use Data::Dump qw( dump );
 
-our $VERSION = '0.75';
+our $VERSION = '0.76';
 
 __PACKAGE__->mk_ro_accessors(
     qw(
@@ -15,6 +15,7 @@ __PACKAGE__->mk_ro_accessors(
         term
         term_re
         is_phrase
+        phrase_terms
         )
 );
 
@@ -44,6 +45,12 @@ Search::Tools::RegEx - regular expressions for terms
 =head2 term_re
 
 =head2 is_phrase
+
+=head2 phrase_terms
+
+If B<is_phrase> is true, B<phrase_terms> will contain an ARRAY ref
+of Search::Tools::RegEx objects, each one representing a term
+in the phrase.
 
 =cut
 
