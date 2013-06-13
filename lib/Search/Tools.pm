e67fe627cd49e5daa7751e88d970b87aa8e65c77
Peter Karman <perl@peknet.com>
2013-06-13 21:49:39 +0000

    Import of KARMAN/Search-Tools-0.96 from CPAN.
    gitpan-cpan-distribution: Search-Tools
    gitpan-cpan-version:      0.96
    gitpan-cpan-path:         KARMAN/Search-Tools-0.96.tar.gz
    gitpan-cpan-author:       KARMAN
    gitpan-cpan-maturity:     released

--- lib/Search/Tools.pm
+++ lib/Search/Tools.pm
@@ -6,7 +6,7 @@ use Carp;
 use Scalar::Util qw( openhandle );
 use File::Basename;
 
-our $VERSION = '0.95';
+our $VERSION = '0.96';
 
 use XSLoader;
 XSLoader::load( 'Search::Tools', $VERSION );
@@ -200,19 +200,21 @@ XS debugging help. Same as using Devel::Peek.
 
 Perl 5.8.3 or later is required. This is for full UTF-8 support.
 
-The following CPAN modules are required:
+The following non-core CPAN modules are required:
 
 =over
 
 =item Rose::Object
 
-=item Search::QueryParser
+=item Rose::ObjectX::CAF
+
+=item Search::Query
 
 =item Data::Dump
 
-=item File::Slurp
-
 =item Encode
+
+=item Encoding::FixLatin
 
 =item Carp
 
